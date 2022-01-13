8db1c2e7e8c311bbf88bd2c67e33623170a7f695
Colin M. Saunders <colinmsaunders@gmail.com>
2022-01-13 14:49:36 -0800

    Fix bug in sample-bot.py

--- wordle/python/sample-bot.py
+++ wordle/python/sample-bot.py
@@ -19,21 +19,22 @@ def get_wordlist():
 
 # this has lots of false positives, only pay attention to 3s
 #
-def could_match(target, last_guess, last_score):
-    for i, ch in enumerate(last_score):
+def could_match(target, guess, feedback):
+    for i, ch in enumerate(feedback):
         if '3' == ch:
-            if target[i] != last_guess[i]:
+            if target[i] != guess[i]:
                 return False
         else:
-            if target[i] == last_guess[i]:
+            if target[i] == guess[i]:
                 return False
     return True
 
 
 def play(state):
     # state looks like: "-----:00000,arose:31112,amend:31211"
-    history = state.split(',')
-    last_guess, last_score = history[-1].split(':')
-    possible = list(filter(lambda x: could_match(x, last_guess, last_score), get_wordlist()))
+    possible = get_wordlist()
+    for pair in state.split(','):
+        guess, feedback = pair.split(':')
+        possible = list(filter(lambda x: could_match(x, guess, feedback), possible))
     return random.choice(possible)
 
